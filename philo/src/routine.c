2aa3203439f940b50351b06b102e50e38bf3fa39
antthoma <antthoma@student.42sp.org.br>
2023-10-01 19:50:35 +0200

    fix: norminette

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 01:36:02 by antthoma          #+#    #+#             */
-/*   Updated: 2023/09/29 01:39:13 by antthoma         ###   ########.fr       */
+/*   Updated: 2023/10/01 19:50:16 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	*routine(t_philosopher *philosopher)
 	{
 		philosopher->config->panic_stop = 1;
 		printf("%d %d died\n",
-			   get_time() - philosopher->created_at, philosopher->id);
+			get_time() - philosopher->created_at, philosopher->id);
 	}
 	philosopher->config->threads_finished++;
 	return (NULL);
